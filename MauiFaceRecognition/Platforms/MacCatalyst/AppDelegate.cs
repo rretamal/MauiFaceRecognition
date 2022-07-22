81b0623bc7af41916ba3dab8db75a3d64a7560fc
Ricardo R <rretamal.dev@gmail.com>
2022-07-22 13:57:09 -0400

    Initial files

---
+++ MauiFaceRecognition/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiFaceRecognition;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
