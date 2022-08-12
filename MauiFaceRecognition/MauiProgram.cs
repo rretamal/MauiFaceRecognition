e0e85e2f1929bd906a3537ac766c6bf5932a694f
Ricardo R <rretamal.dev@gmail.com>
2022-08-12 13:53:22 -0400

    Add Camera permissions

--- MauiFaceRecognition/MauiProgram.cs
+++ MauiFaceRecognition/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiFaceRecognition;
+﻿using Microsoft.Maui.LifecycleEvents;
+
+namespace MauiFaceRecognition;
 
 public static class MauiProgram
 {
@@ -7,6 +9,46 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+            .ConfigureLifecycleEvents(life =>
+            {
+#if __ANDROID__
+                    Platform.Init(MauiApplication.Current);
+
+                    life.AddAndroid(android => android
+                        .OnRequestPermissionsResult((activity, requestCode, permissions, grantResults) =>
+                        {
+                            Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+                        })
+                        .OnNewIntent((activity, intent) =>
+                        {
+                            Platform.OnNewIntent(intent);
+                        })
+                        .OnResume((activity) =>
+                        {
+                            Platform.OnResume();
+                        }));
+#elif __IOS__
+                //life.AddiOS(ios => ios
+                //	.ContinueUserActivity((application, userActivity, completionHandler) =>
+                //	{
+                //		return Platform.ContinueUserActivity(application, userActivity, completionHandler);
+                //	})
+                //	.OpenUrl((application, url, options) =>
+                //	{
+                //		return Platform.OpenUrl(application, url, options);
+                //	})
+                //	.PerformActionForShortcutItem((application, shortcutItem, completionHandler) =>
+                //	{
+                //		Platform.PerformActionForShortcutItem(application, shortcutItem, completionHandler);
+                //	}));
+#elif WINDOWS
+				//life.AddWindows(windows => windows
+				//	.OnLaunched((application, args) =>
+				//	{
+				//		Platform.OnLaunched(args);
+				//	}));
+#endif
+            })
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
