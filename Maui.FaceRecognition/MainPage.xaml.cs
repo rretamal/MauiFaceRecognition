0932eed7e1ecebbb278860494ddd495af1cd0a2e
Ricardo R <1440299+rretamal@users.noreply.github.com>
2022-09-06 20:53:53 -0400

    Capture frames

--- Maui.FaceRecognition/MainPage.xaml.cs
+++ Maui.FaceRecognition/MainPage.xaml.cs
@@ -1,14 +1,37 @@
-﻿namespace Maui.FaceRecognition;
+﻿using Maui.FaceRecognition.Core;
+
+namespace Maui.FaceRecognition;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+    FaceClassifier classifier;
+    byte[] frame;
 
 	public MainPage()
 	{
 		InitializeComponent();
+
+        classifier = new FaceClassifier();
+
+        btnTakePicture.Clicked += BtnTakePicture_Clicked;
+        cameraView.FrameReady += CameraView_FrameReady;
 	}
 
+    private void CameraView_FrameReady(object sender, ZXing.Net.Maui.CameraFrameBufferEventArgs e)
+    {
+        var bytes = new Byte[e.Data.Data.Remaining()];
+        e.Data.Data.Get(bytes, 0, bytes.Length);
+        
+        
+    }
+
+    private void BtnTakePicture_Clicked(object sender, EventArgs e)
+    {
+        
+        //cameraView.Frame.
+        //classifier.ExtractEmbeddings()
+    }
 }
 
 
